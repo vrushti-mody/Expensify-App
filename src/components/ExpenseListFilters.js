8f47a647c0244684c170b5ec8b861ce22363430f
vrushti-mody <vrushtimody6@gmail.com>
2021-02-12 00:37:11 +0530

    Filtering by Dates

--- src/components/ExpenseListFilters.js
+++ src/components/ExpenseListFilters.js
@@ -1,28 +1,54 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import {connect} from 'react-redux'
-import {setTextFilter, sortByAmount, sortByDate} from '../actions/filters'
+import {setTextFilter, sortByAmount, sortByDate, setStartDate, setEndDate} from '../actions/filters'
+import {DateRangePicker} from 'react-dates'
 
-const ExpenseListFilters = (props)=>(
+class ExpenseListFilters extends React.Component{
+    state={
+        focused: null
+    }
+    onDatesChange=({startDate,endDate})=>{
+        this.props.dispatch(setStartDate(startDate))
+        this.props.dispatch(setEndDate(endDate))
+    }
+    onFocusChange=(focused)=>{
+        this.setState(()=>({focused}))
+    }
+    render(){
+        return(
     <div>
       <input 
       type="text" 
-      value={props.filters.text} 
+      value={this.props.filters.text} 
       onChange={(e)=>{
-          props.dispatch(setTextFilter(e.target.value))
+          this.props.dispatch(setTextFilter(e.target.value))
           console.log(e.target.value)
       }} ></input>
       <select onChange={
           (e)=>{
-            e.target.value==='date' ? props.dispatch(sortByDate()) : props.dispatch(sortByAmount())
+            e.target.value==='date' ? this.props.dispatch(sortByDate()) : this.props.dispatch(sortByAmount())
            
         }
       }>
           <option value="date">Date</option>
           <option value="amount">Amount</option>
-      </select>
+      </select> 
+      <DateRangePicker
+      startDate={this.props.filters.startDate}
+      endDate={this.props.filters.endDate}
+      onDatesChange={this.onDatesChange}
+      focusedInput={this.state.focused}
+      onFocusChange={this.onFocusChange}
+      numberOfMonths={1}
+      isOutsideRange={()=>false}
+      showClearDates={true}
+      
+      />
     </div>
-)
+    )
+    }
+}
 
 const mapStateToProps =  (state)=>{
     return{
