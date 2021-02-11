d718537a192209b3e6e9f056884c342721199b91
vrushti-mody <vrushtimody6@gmail.com>
2021-02-12 02:27:05 +0530

    Set up dev dependencies and dist

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ return(
     
     entry:'./src/app.js',
     output:{
-        path:path.join(__dirname,'/public'),
+        path:path.join(__dirname,'public','dist'),
        
         filename:'bundle.js'
     },
@@ -49,7 +49,8 @@ return(
 
     devServer:{
         contentBase:path.join(__dirname,'/public'),
-        historyApiFallback:true
+        historyApiFallback:true,
+        publicPath:'/dist/'
     },
     devtool: isProduction ? 'source-map':'inline-source-map',
     plugins:[
