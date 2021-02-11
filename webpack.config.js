0cff4f39bc2615b1a0a2f073524b8f0a7d5d8f6a
vrushti-mody <vrushtimody6@gmail.com>
2021-02-12 02:01:18 +0530

    Added seperate css files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 //entry -> endpoint
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports= (env)=>{
     console.log(env)
     const isProduction = env =='production'
+    const CSSExtract = new ExtractTextPlugin('styles.css')
     console.log(isProduction)
 return(
 {
@@ -21,7 +23,23 @@ return(
             exclude: /node_modules/,
         },{
             test:/\.(s?)css$/,
-            use:['style-loader','css-loader','sass-loader'],
+            use:CSSExtract.extract({
+                use:[
+                    {
+                        loader:'css-loader',
+                        options:{
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader:'sass-loader',
+                        options:{
+                            sourceMap: true
+                        }
+                    }
+                    
+                ]
+            }),
             include: [
                 path.resolve(__dirname, "./src"),
                 path.resolve(__dirname, "./node_modules")
@@ -33,7 +51,10 @@ return(
         contentBase:path.join(__dirname,'/public'),
         historyApiFallback:true
     },
-    devtool: isProduction ? 'source-map':'cheap-module-eval-source-map'
+    devtool: isProduction ? 'source-map':'inline-source-map',
+    plugins:[
+        CSSExtract
+    ]
     
 });
 }
